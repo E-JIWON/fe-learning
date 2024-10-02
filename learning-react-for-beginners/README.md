b3251d57f333fa4fadf69ddeab3ddbd8847cd520
EZONE <meowoof01@gmail.com>
2024-10-02 13:56:48 +0900

    first commit
    - first commit
    - useEffect 강의까지 학습
      https://nomadcoders.co/react-for-beginners/lectures/3284

---
+++ learning-react-for-beginners/README.md
@@ -0,0 +1,13 @@
+# React Beginners
+
+이 프로젝트는 React 기초를 잘 다지기 위해 만든 레포입니다.
+
+노마드 코더의 [ReactJS로 영화 웹 서비스 만들기](https://nomadcoders.co/react-for-beginners) 강의를 하였습니다.
+
+## 실행 방법
+
+프로젝트를 실행하려면 다음 명령어를 입력하세요:
+
+```bash
+npm start
+```
---
+++ learning-react-for-beginners/src/App.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("call the API...");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("search for ", keyword);
+    }
+  }, [keyword]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search here..."
+        value={keyword}
+        onChange={onChange}
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
